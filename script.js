2b9072ebd1439dca9177577607b4acf960cc5a34
Thomas Andersen <tja_skb@hotmail.com>
2021-03-25 21:38:27 +0100

    alert number at 0 input

--- script.js
+++ script.js
@@ -15,8 +15,14 @@ function simpleInterest() {
   var termOfLoan = parseFloat(document.getElementById("year").value);
   var simpleInt = principal * interestRate * termOfLoan;
   var amount = (principal + simpleInt).toFixed(2);
-  document.getElementById("output-01").innerHTML = "If you deposit " + principal;
-  document.getElementById("output-02").innerHTML = "at an interest rate of " + (interestRate * 100).toPrecision(2) + "%";
-  document.getElementById("output-03").innerHTML = "You will recieve an amount of " + simpleInt.toFixed(2);
-  document.getElementById("output-04").innerHTML = "In the year " + (n + termOfLoan);
+  if (principal == 0) {
+    alert("Enter a numnber")
+  } else {
+    document.getElementById("output-01").innerHTML = "If you deposit " + principal;
+    document.getElementById("output-02").innerHTML = "at an interest rate of " + (interestRate * 100).toPrecision(2) + "%";
+    document.getElementById("output-03").innerHTML = "You will recieve an amount of " + simpleInt.toFixed(2);
+    document.getElementById("output-04").innerHTML = "In the year " + (n + termOfLoan);
+  }
+  
 }
+
